c6ecd2950723e32d6dab2892e9537515a567d620
trandinhvinh_qmv <trandinhvinh@gmail.com>
2022-04-16 22:40:14 +0700

    [] - Add data acess to project

---
+++ BrainCoffeeShop.DataAcess/Model/CoffeeShop.cs
@@ -0,0 +1,8 @@
+﻿namespace BrainCoffeeShop.DataAcess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeanInStockInKg { get; set; }
+    }
+}
